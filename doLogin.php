c796a88e6004aa4ebe717474f3cc5ab1a086921e
nadopro <nadopro@gmail.com>
2026-04-14 13:54:38 +0900

    0414g

--- doLogin.php
+++ doLogin.php
@@ -3,19 +3,18 @@
     $id = $_POST["id"];
     $pass = $_POST["pass"];
 
-    if($id == "admin" and $pass=="1111")
+    $sql = "select * from users where id='$id' and pass='$pass' ";
+    $result = mysqli_query($conn, $sql);
+    $data = mysqli_fetch_array($result);
+
+    if($data)
     {
         $_SESSION["cnuid"] = $id;
-        $_SESSION["cnuname"] = "관리자";
-        $_SESSION["cnulevel"] = 9;
-        $msg = "관리자님 반갑습니다.";
-    } else if($id == "test" and $pass=="1111")
-    {
-        $_SESSION["cnuid"] = $id;
-        $_SESSION["cnuname"] = "홍길동";
-        $_SESSION["cnulevel"] = 1;
-        $msg = "홍길동님 반갑습니다.";
-    }else
+        $_SESSION["cnuname"] = $data["name"];
+        $name = $data["name"];
+        $_SESSION["cnulevel"] = $data["level"];
+        $msg = "$name 님 반갑습니다.";
+    } else
     {
         $msg = "아이디와 비밀번호를 확인하세요";
     }
